ea18a79f74da740079cce11ed6971e23428f0cbf
Headbolt <54707369+Headbolt@users.noreply.github.com>
2019-11-04 17:48:19 +0000

    Update JAMF-Ext-Recover.sh

--- JAMF-Ext-Recover.sh
+++ JAMF-Ext-Recover.sh
@@ -12,15 +12,18 @@
 #
 # HISTORY
 #
-#   Version: 1.2 - 04/11/2019
+#   Version: 1.3 - 04/11/2019
 #
 #   - 14/10/2018 - V1.0 - Created by Headbolt
 #
 #   - 01/04/2019 - V1.1 - Updated by Headbolt
-#				Updated for Mojave
-#   - 04/11/2019 - V1.2 - Updated by Headbolt
-#				Updated for Catalina and later by checking for the 
-#				crossover points rather than individual OS Versions
+#							Updated for Mojave
+#   - 03/11/2019 - V1.2 - Updated by Headbolt
+#							Updated for Catalina and later by checking for the 
+#							crossover points rather than individual OS Versions
+#   - 04/11/2019 - V1.3 - Updated by Headbolt
+#							Updated Again to Cycle through all instances of Recovery Partitions
+#							and report the Highest Version Number as what is available
 #
 ###############################################################################################################################################
 #
@@ -29,11 +32,11 @@ MajorVer=$(/bin/echo "$OS_ver" | awk -F. '{ print $1; }') # Split Out Major Vers
 MinorVer=$(/bin/echo "$OS_ver" | awk -F. '{ print $2; }') # Split Out Minor Version
 PatchVer=$(/bin/echo "$OS_ver" | awk -F. '{ print $3; }') # Split Out Patch Version
 #
-if [[ "${MinorVer}" -lt 12 ]] # Check Minor Version is Sierra or Lower
+if [[ "${MinorVer}" -le 12 ]] # Check Minor Version is Sierra or Lower
 	then
-		disk=0 # If Sierra or Lower we need to look at Disk 0
+		disk="disk0" # If Sierra or Lower we need to look at Disk 0
 	else
-		disk=1 # If High Sierra or Higher we need to look at Disk 1
+		disk="disk1" # If High Sierra or Higher we need to look at Disk 1
 fi
 #
 # Check for Relevant Recovery Partition
@@ -41,7 +44,42 @@ recoveryHDPresent=$(/usr/sbin/diskutil list | grep "Recovery" | grep $disk)
 #
 if [ "$recoveryHDPresent" != "" ] # Check and Output presence of Recovery Partition
 	then
-		/bin/echo "<result>Present</result>"
+		recoveryPartition=$(/bin/echo "$recoveryHDPresent" | rev | cut -c -7 | rev)
+		RecoveryVolumeMountMessage=$(diskutil mount $recoveryPartition)
+		RecoveryVolumeMountPoint=$(echo $RecoveryVolumeMountMessage | awk -F"on" '{ print $1; }' | cut -c 8- | rev | cut -c 2- | rev)
+		RecoveryVolumeFolderList=$(ls /Volumes/"$RecoveryVolumeMountPoint"/)
+		#
+		BestRecVer="0"
+		for RecPath in $RecoveryVolumeFolderList
+			do
+				# Grab the Version from this instance of Recovery
+				RecVer=$(/usr/bin/defaults read /Volumes/"$RecoveryVolumeMountPoint"/$RecPath/SystemVersion.plist ProductVersion)
+				RecMajorVer=$(/bin/echo "$RecVer" | awk -F. '{ print $1; }') # Split Out Major Version
+				RecMinorVer=$(/bin/echo "$RecVer" | awk -F. '{ print $2; }') # Split Out Minor Version
+				RecPatchVer=$(/bin/echo "$RecVer" | awk -F. '{ print $3; }') # Split Out Patch Version
+				#
+				if [[ "${BestRecVer}" == "0" ]]
+					then
+						BestRecVer=$RecVer
+					else
+						BestRecMajorVer=$(/bin/echo "$BestRecVer" | awk -F. '{ print $1; }') # Split Out Major Version
+						BestRecMinorVer=$(/bin/echo "$BestRecVer" | awk -F. '{ print $2; }') # Split Out Minor Version
+						BestRecPatchVer=$(/bin/echo "$BestRecVer" | awk -F. '{ print $3; }') # Split Out Patch Version
+						#
+						if [[ "${BestRecMajorVer}" -le "${RecMajorVer}" ]]
+							then
+								if [[ "${BestRecMinorVer}" -le "${RecMinorVer}" ]]
+									then	
+										if [[ "${BestRecPatchVer}" -le "${RecPatchVer}" ]]
+											then
+												BestRecVer=$RecVer
+										fi
+								fi
+						fi
+				fi
+			done
+		#
+		/bin/echo "<result>$BestRecVer</result>"
 	else
 		/bin/echo "<result>Not Present</result>"
 fi    
